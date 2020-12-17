bd1ca2e01da5d16d71df10743c4a44e5d62bee0e
leenajyotishi <69509483+leenajyotishi@users.noreply.github.com>
2020-12-17 06:56:12 -0500

    Commit

---
+++ app.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask,render_template
+import cv2 #for image processing
+import easygui #to open the filebox
+import numpy as np #to store image
+import imageio #to read image stored at particular path
+import sys
+import matplotlib.pyplot as plt
+import os
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import ImageTk, Image
+import random
+""" fileopenbox opens the box to choose file
+and help us store file path as string """
+def upload():
+    ImagePath=easygui.fileopenbox()
+    cartoonify(ImagePath)
+    
+
+
+app=Flask(__name__)
+@app.route('/')
+def home():
+    ImagePath=easygui.fileopenbox()
+    originalmage = cv2.imread(ImagePath)
+    originalmage = cv2.cvtColor(originalmage, cv2.COLOR_BGR2RGB)
+#print(image)  # image is stored in form of numbers
+# confirm that image is chosen
+    if originalmage is None:
+        print("Can not find any image. Choose appropriate file")
+        sys.exit()
+    ReSized1 = cv2.resize(originalmage, (960, 540))
+    #plt.imshow(ReSized1, cmap='gray')
+    
+    grayScaleImage = cv2.cvtColor(originalmage, cv2.COLOR_BGR2GRAY)
+    ReSized2 = cv2.resize(grayScaleImage, (960, 540))
+    
+    
+    smoothGrayScale = cv2.medianBlur(grayScaleImage, 5)
+    ReSized3 = cv2.resize(smoothGrayScale, (960, 540)) 
+    
+    getEdge = cv2.adaptiveThreshold(smoothGrayScale, 255, 
+    cv2.ADAPTIVE_THRESH_MEAN_C, 
+    cv2.THRESH_BINARY, 9, 9)
+    ReSized4 = cv2.resize(getEdge, (960, 540))
+    
+    colorImage = cv2.bilateralFilter(originalmage, 9, 300, 300)
+    ReSized5 = cv2.resize(colorImage, (960, 540))
+    
+    cartoonImage = cv2.bitwise_and(colorImage, colorImage, mask=getEdge)
+    ReSized6 = cv2.resize(cartoonImage, (960, 540))
+    
+    
+    # Plotting the whole transition
+    images=[ReSized1, ReSized2, ReSized3, ReSized4, ReSized5, ReSized6]
+    fig, axes = plt.subplots(3,2, figsize=(8,8), subplot_kw={'xticks':[], 'yticks':[]}, gridspec_kw=dict(hspace=0.1, wspace=0.1))
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(images[i], cmap='gray')
+       
+    #plt.show()
+    #plt.imshow(images, cmap='gray')
+    plt.imshow(ReSized6, cmap='gray')
+    
+    rannum=random.randint(0,1000)
+    plt.savefig('static/images/' + str(rannum) +'.png')
+
+    #return render_template('plot.html', url='/static/images/plot.png')
+    return render_template('plot.html', name = 'plot3', url ='/static/images/'+ str(rannum) +'.png')
+
+
+def cartoonify(ImagePath):
+    #read the image
+    originalmage = cv2.imread(ImagePath)
+    originalmage = cv2.cvtColor(originalmage, cv2.COLOR_BGR2RGB)
+#print(image)  # image is stored in form of numbers
+# confirm that image is chosen
+    if originalmage is None:
+        print("Can not find any image. Choose appropriate file")
+        sys.exit()
+    ReSized1 = cv2.resize(originalmage, (960, 540))
+    
+    grayScaleImage = cv2.cvtColor(originalmage, cv2.COLOR_BGR2GRAY)
+    ReSized2 = cv2.resize(grayScaleImage, (960, 540))
+   # plt.imshow(ReSized1, cmap='gray')
+    
+if __name__ == "__main__":
+     app.run(debug=True)
