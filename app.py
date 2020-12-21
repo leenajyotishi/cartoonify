326d1d6bb3db5e0c746f3236cc78b96c58495033
leenajyotishi <69509483+leenajyotishi@users.noreply.github.com>
2020-12-21 02:58:33 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,42 +1,72 @@
-from flask import Flask, request, jsonify, render_template,redirect,url_for
-import pandas as pd
-from fbprophet import Prophet
+#opencv-python==4.4.0.46
+
+from flask import Flask,render_template
+import cv2
+import easygui #to open the filebox
+import numpy as np #to store image
+import imageio #to read image stored at particular path
+import sys
+import matplotlib.pyplot as plt
+import os
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import ImageTk, Image
+import random
+""" fileopenbox opens the box to choose file
+and help us store file path as string """
 app=Flask(__name__)
+
+
 
 @app.route('/')
 def home():
-    return render_template('Main.html')
+    ImagePath=easygui.fileopenbox()
+    originalmage = cv2.imread(ImagePath)
+    originalmage = cv2.cvtColor(originalmage, cv2.COLOR_BGR2RGB)
+#print(image)  # image is stored in form of numbers
+# confirm that image is chosen
+    if originalmage is None:
+        print("Can not find any image. Choose appropriate file")
+        sys.exit()
+    ReSized1 = cv2.resize(originalmage, (960, 540))
+    #plt.imshow(ReSized1, cmap='gray')
     
-@app.route('/data',methods=['POST'])        
-def data():
-   
-    Noofmonths=int(request.form['Noofmonths'])
-    Productnm=request.form['Product']
-    PriceQ1=int(request.form['Quarter1'])
-    PriceQ2=int(request.form['Quarter2'])
-    PriceQ3=int(request.form['Quarter3'])
-    PriceQ4=int(request.form['Quarter4'])
-    data1 = {'Product':[Productnm],'2020-01-01':[PriceQ1],'2020-03-01':[PriceQ2],'2020-06-01':[PriceQ3],
-       '2020-09-01':[PriceQ4]}
-    df1 = pd.DataFrame(data1)
-    gapminder_tidy = df1.melt(id_vars=["Product"], 
-                              var_name="year", 
-                              value_name="Amount")
-    df = gapminder_tidy.rename(columns={'year': 'ds', 'Amount':'y'})
-    grouped = df.groupby('Product')
-    final = pd.DataFrame()
-    for g in grouped.groups:
-        group = grouped.get_group(g)
-        m = Prophet()
-        m.fit(group)
-        future = m.make_future_dataframe(periods=Noofmonths, freq='M')
-        forecast = m.predict(future)    
-        forecast = forecast.rename(columns={'yhat': g})
-        final = pd.merge(final, forecast.set_index('ds'), how='outer', left_index=True, right_index=True)
-    final = final[[ g for g in grouped.groups.keys()]]
-    dff = pd.DataFrame(final)
-    dff = dff.rename(columns={'ds': 'd'})
-    return render_template('data.html', data=dff.to_html(classes='form-group'))
+    grayScaleImage = cv2.cvtColor(originalmage, cv2.COLOR_BGR2GRAY)
+    ReSized2 = cv2.resize(grayScaleImage, (960, 540))
+    
+    
+    smoothGrayScale = cv2.medianBlur(grayScaleImage, 5)
+    ReSized3 = cv2.resize(smoothGrayScale, (960, 540)) 
+    
+    getEdge = cv2.adaptiveThreshold(smoothGrayScale, 255, 
+    cv2.ADAPTIVE_THRESH_MEAN_C, 
+    cv2.THRESH_BINARY, 9, 9)
+    ReSized4 = cv2.resize(getEdge, (960, 540))
+    
+    colorImage = cv2.bilateralFilter(originalmage, 9, 300, 300)
+    ReSized5 = cv2.resize(colorImage, (960, 540))
+    
+    cartoonImage = cv2.bitwise_and(colorImage, colorImage, mask=getEdge)
+    ReSized6 = cv2.resize(cartoonImage, (960, 540))
+    
+    
+    # Plotting the whole transition
+    images=[ReSized1, ReSized2, ReSized3, ReSized4, ReSized5, ReSized6]
+    fig, axes = plt.subplots(3,2, figsize=(8,8), subplot_kw={'xticks':[], 'yticks':[]}, gridspec_kw=dict(hspace=0.1, wspace=0.1))
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(images[i], cmap='gray')
+       
+    #plt.show()
+    #plt.imshow(images, cmap='gray')
+    plt.imshow(ReSized6, cmap='gray')
+    
+    rannum=random.randint(0,1000)
+    plt.savefig('static/images/' + str(rannum) +'.png')
+
+    #return render_template('plot.html', url='/static/images/plot.png')
+    return render_template('plot.html', name = 'plot3', url ='/static/images/'+ str(rannum) +'.png')
+
 
 
 if __name__ == "__main__":
